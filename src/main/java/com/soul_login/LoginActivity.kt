ad8bc387163b1745f5f128dd6705480b6b63b9d2
chanzhilin <1181785848@qq.com>
2019-06-20 17:22:33 +0800

    格式

--- src/main/java/com/soul_login/LoginActivity.kt
+++ src/main/java/com/soul_login/LoginActivity.kt
@@ -1,15 +1,26 @@
 package com.soul_login
 
-import android.os.Bundle
-import android.support.v7.app.AppCompatActivity
 import com.alibaba.android.arouter.facade.annotation.Route
 import com.kotlin_baselib.api.Constants
+import com.kotlin_baselib.base.BaseActivity
+import com.kotlin_baselib.base.EmptyModelImpl
+import com.kotlin_baselib.base.EmptyPresenterImpl
+import com.kotlin_baselib.base.EmptyView
 
 @Route(path = Constants.LOGIN_ACTIVITY_PATH)
-class LoginActivity : AppCompatActivity() {
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+class LoginActivity : BaseActivity<EmptyView, EmptyModelImpl, EmptyPresenterImpl>(), EmptyView {
+    override fun createPresenter(): EmptyPresenterImpl {
+        return EmptyPresenterImpl(this)
     }
+
+    override fun getResId(): Int {
+        return R.layout.activity_login
+    }
+
+    override fun initData() {
+    }
+
+    override fun initListener() {
+    }
+
 }
